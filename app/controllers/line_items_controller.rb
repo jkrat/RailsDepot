65dfea27463d8c1c1b741d3483ee5a6c03c3bd80
Jim Kratus <jimkratus@gmail.com>
2019-09-18 15:34:14 -0500

    ajax call to add item to cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -26,13 +26,13 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
-    session[:counter] = 0
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
+        format.html { redirect_to store_index_url }
+        format.js
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,7 @@
 class StoreController < ApplicationController
+  include CurrentCart
   include StoreCounter
+  before_action :set_cart
   before_action :set_counter
   def index
     @products = Product.order(:title)
